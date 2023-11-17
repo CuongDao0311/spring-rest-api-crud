a96c4768b5318db9d70d538257380d8a7a127854
CuongDao0311 <112787247+CuongDao0311@users.noreply.github.com>
2023-11-17 09:48:33 +0700

    Add files via upload

---
+++ main-04-spring-boot-rest-crud-employee-jpa-api/src/main/java/com/example/demo/dao/EmployeeDAOImpl.java
@@ -0,0 +1,48 @@
+package com.example.demo.dao;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.entity.Employee;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+
+@Repository
+public class EmployeeDAOImpl implements EmployeeDAO {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public EmployeeDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+
+        TypedQuery<Employee> query = entityManager.createQuery(
+                "from Employee",
+                Employee.class);
+
+        return query.getResultList();
+    }
+
+    @Override
+    public Employee findById(int id) {
+        return entityManager.find(Employee.class, id);
+    }
+
+    @Override
+    public Employee save(Employee employee) {
+        return entityManager.merge(employee);
+    }
+
+    @Override
+    public void deleteById(int id) {
+        Employee employee = findById(id);
+        entityManager.remove(employee);
+    }
+}
